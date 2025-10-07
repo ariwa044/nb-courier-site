b1f01e0a16977e10e77f215ab29a890836e3bf51
ariwa044 <pt37288997@gmail.com>
2025-10-07 16:06:35 +0100

    update

--- config/settings.py
+++ config/settings.py
@@ -161,6 +161,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+CSRF_COOKIE_SECURE = True
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
